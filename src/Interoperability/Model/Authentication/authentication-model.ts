6a39521ded36bcb2a4fa0c31374d6ec263b6fa67
Internet Things <iot@seiko-it.com.ph.local>
2022-11-05 23:31:46 +0800

    create package structure and add authentication function

---
+++ src/Interoperability/Model/Authentication/authentication-model.ts
@@ -0,0 +1,14 @@
+export declare type Headers = {
+    method      : string,
+    apikey      : string,
+    key         : string,
+    token?      : number,
+    ContentType : string
+}
+
+export declare type Credentials = {
+    username?    :   string,
+    password?    :   string,
+    apikey?      :   string,
+    token?       :   string
+}
